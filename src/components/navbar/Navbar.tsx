d211033eb56936a09b0a7df4767b3542d931bec2
Your Name <Yoshimitsu247Kakarot@gmail.com>
2025-10-23 15:19:40 -0600

    Navbr mobile responsiveness

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -17,12 +17,12 @@ function Navbar() {
 
     return (
         <div className='w-full'>
-            <div className='w-full py-12 flex justify-center items-center'>
+            <div className='w-full py-8 md:py-12 flex justify-center items-center'>
                 <div className='w-fit flex gap-10'>
                     <div className='flex items-center gap-4'>
-                        <Image className='w-[100px] h-[100px] object-cover' src={logoMark} alt="Main logo of rccg" />
+                        <Image className='w-[70px] md:w-[100px] h-[70px] md:h-[100px] object-cover' src={logoMark} alt="Main logo of rccg" />
                         <div className='w-1 h-[100px] border-l-2 border-l-blue-600'></div>
-                        <Image className='w-[200px] h-[100px]' src={parishLogoW} alt="Main logo of House of peace parish" />
+                        <Image className='w-[100px] md:w-[200px] h-[50px] md:h-[100px]' src={parishLogoW} alt="Main logo of House of peace parish" />
                     </div>
                     <div className='hidden md:flex gap-10'>
                         {
@@ -39,23 +39,18 @@ function Navbar() {
                     </div>
                 </div>
             </div>
-
             <div className="w-full py-6 md:py-10 px-6 md:px-24 flex justify-between items-center bg-[#3B82F6] relative">
                 {/* Hamburger Icon */}
                 <div
                     onClick={handleNav}
-                    className="border-2 border-[#F5F5F5] p-2 rounded-md cursor-pointer md:hidden z-30"
+                    className="border-2 border-[#F5F5F5] p-2 rounded-md cursor-pointer md:hidden"
                 >
                     <GiHamburgerMenu className="text-[#F5F5F5]" size={20} />
                 </div>
 
                 {/* Navigation Links */}
-                <div className={`
-      fixed top-0 left-0 h-full bg-[#3B82F6] flex flex-col items-center justify-center gap-6 
-      text-[#F5F5F5] font-semibold text-lg transform transition-transform duration-700 ease-in-out
-      ${openNav ? 'translate-x-0 w-[70vw] z-20' : '-translate-x-full'}
-      md:static md:flex md:flex-row md:h-auto md:w-auto md:translate-x-0 md:gap-8
-    `}
+                <div
+                    className={`fixed top-0 left-0 h-full bg-[#3B82F6] flex flex-col items-center justify-center gap-6  text-[#F5F5F5] font-semibold text-lg transform transition-transform duration-700 ease-in-out ${openNav ? 'translate-x-0 w-[70vw] z-20' : '-translate-x-full'} md:static md:flex md:flex-row md:h-auto md:w-auto md:translate-x-0 md:gap-8`}
                 >
                     <Link href="/">Home</Link>
                     <Link href="/live">Live</Link>
